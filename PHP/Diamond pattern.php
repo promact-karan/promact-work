e8bd00e5e8e8547a0d79e74c44f29027bc920323
Karan Mehta <karanmehta@promact.com>
2017-02-18 12:59:10 +0530

    completed

--- PHP/Diamond pattern.php
+++ PHP/Diamond pattern.php
@@ -1,107 +1,39 @@
 <?php
-		//echo "Enter Number of Rows:";
-		$x = 11;
-		$y=$x/2;
-		$z=$y;
-		$no=1;
-		$k=$x;
-		/*for($i=1;$i<=$y;$i++)
-		{
-			for($k=1;$k<($z/2);$k++)
-			{
-				echo(" ");
-			}
-			$z=$z-1;
-			for($j=1;$j<=$i;$j++)
-			{
-				echo("*");
-			}
-			echo("\n");
-		}
-		$z=$y;
-		for($i=1;$i<=$y;$i++)
-		{
-			for($k=1;$k<($z/2);$k++)
-			{
-				echo(" ");
-			}
-			$z=$z-1;
-			for($j=$y;$j>=$i;$j--)
-			{
-				echo("*");
-			}
-			echo("\n");
-		}
-		*/
-		/*for($i=1;$i<=$x;$i++)
-		{
-			
-			if($i%2!=0)
-			{
-			echo($no++);
-				for($k=0;$k<=$z;$k++)
-				{
-					echo (" ");
-				}
-				$z--;
-				for($j=1;$j<=$i;$j++)
-				{	
-					echo("*");
-				}
-			}
-			echo("\n");
-		}
-		for($i=1;$i<=$x;$i++)
-		{
-			
-			if($i%2==0)
-			{
-			echo($no++);
-				for($k=x;$k>=$z;$k--)
-				{
-					echo (" ");
-				}
-				$z++;
-				for($j=x;$j<=$i;$j--)
-				{	
-					echo("*");
-				}
-			}
-			echo("\n");
-		}*/
-		function read_stdin(){
-    
-    $give=fopen("php://stdin","r");
-    $input=fgets($give,100);
-    $input=rtrim($input);
-    fclose($give);
-    return $input;
-
-}
-echo "Enter Number of Rows::";
-$input= read_stdin();
-$temp=$input;
-
-for($i=0;$i<$input;$i++)
-{
-	if($i/2!=0)
+<?php
+	$n= Readline("Enter Number of rows::");
+	$m=$n;
+	for($i=0;$i<$n;$i++)
 	{
+		if($i%2==0)
+		{
+		for($k=0;$k<($m/2);$k++)
+		{
+			echo(" ");
+		}
+		$m=$m-2;
 		for($j=0;$j<=$i;$j++)
+		{
+			echo("*");
+		}
+		echo("\n");
+		}
+	}
+	for($x=($n-2);$x>=0;$x--)
 	{
-	echo "*";	
+		if($x%2==0)
+		{
+			$m=$m+2;
+			for($k=0;$k<=($m/2) ;$k++)
+			{
+				echo(" ");
+			}
+			echo(" ");
+			for($j=0;$j<=$x;$j++)
+			{
+				echo("*");
+			}
+		echo("\n");
+		}
 	}
-	echo " \n";
-	}
-	
-}	
-for($i=$input;$i>=0;$i--)
-{
-	
-	for($j=$i;$j>=0;$j--)
-	{
-	echo "*";	
-	}
-	echo " \n";
-}
-		
-	?>
+?>
+?>
